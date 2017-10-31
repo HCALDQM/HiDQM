bc71bde9f0eb9d03f3b765f10f757a6c1d2c10d3
DryRun <david.renhwa.yu@gmail.com>
2017-10-31 15:32:45 +0100

    Still trying for first working example

--- js/index.js
+++ js/index.js
@@ -133,7 +133,7 @@ $(document).ready(
 		collec_file = "collections_"+$("#year").val()+".json";
 		console.log("collection file  : " + collec_file);
 		
-		$.getJSON("collections_2017.json", function (data) {
+		$.getJSON("collections_2016.json", function (data) {
 			//$.getJSON(collec_file, function (data) {
 			collections = data;
 		});
